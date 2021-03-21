6cbbc221123beaa0a4d4b198e7136b432739e3e8
RaymondThornley <Raymond@Thornley.net>
2021-03-21 13:36:22 -0500

    Implemented assignment 2

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -1,5 +1,48 @@
-(function(){
+(function () {
+    "use strict";
+
     angular.module("ShoppingListCheckOff", [])
-        .controller("ToBuyController", toBuyControllerFunction)
-        .controller("AlreadyBoughtController", alreadyBoughtControllerFunction);
+        .controller("ToBuyController", ToBuyController)
+        .controller("AlreadyBoughtController", AlreadyBoughtController)
+        .service("ShoppingListCheckOffService", ShoppingListCheckOffService);
+
+    ToBuyController.$inject = ['ShoppingListCheckOffService'];
+    function ToBuyController(ShoppingListCheckOffService) {
+        const toBuy = this;
+        toBuy.itemList = ShoppingListCheckOffService.getToBuyList();
+
+        toBuy.buyItem = function (index) {
+            ShoppingListCheckOffService.buyItem(index);
+        }
+    };
+
+    AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+    function AlreadyBoughtController(ShoppingListCheckOffService) {
+        const alreadyBought = this;
+        alreadyBought.itemList = ShoppingListCheckOffService.getBoughtList();
+    };
+
+    function ShoppingListCheckOffService() {
+        const service = this;
+        let toBuyList = [
+            { name: "cookies", quantity: "10" },
+            { name: "chips", quantity: "5" },
+            { name: "soda", quantity: "20" },
+            { name: "candy", quantity: "5" },
+            { name: "ice cream", quantity: "25" }
+        ];
+        let boughtList = [];
+
+        service.getToBuyList = function () {
+            return toBuyList;
+        };
+
+        service.getBoughtList = function () {
+            return boughtList;
+        };
+
+        service.buyItem = function (index) {
+            boughtList.push(toBuyList.splice(index, 1)[0]);
+        };
+    }
 })();
